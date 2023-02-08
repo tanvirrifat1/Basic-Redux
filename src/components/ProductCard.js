ce44d92a2c190d8aa567780877bf806d97e8f705
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-02-08 14:34:14 +0600

    added more

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
-import { addToCart } from "../redux/actionCreators/productActions";
+import { addToCart, removeFromCart } from "../redux/actionCreators/productActions";
 import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 import { actionTypes } from "../state/ProductState/actionTypes";
+import { AiFillDelete } from "react-icons/ai";
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-
   const dispatch = useDispatch();
+  const { pathName } = useLocation()
 
   return (
     <div
@@ -27,19 +29,29 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
+
+        {<button
+          onClick={() => dispatch(removeFromCart(product))}
+          className='text-red-500 text-2xl font-bold'>
+          <AiFillDelete />
+        </button>}
+
         <button
           onClick={() => dispatch(addToCart(product))}
           className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
         </button>
+
+
         <button
           title='Add to wishlist'
           className='bg-indigo-500  py-1 px-2 rounded-full'
         >
           <BiListPlus className='text-white' />
         </button>
+
       </div>
-    </div>
+    </div >
   );
 };
 
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -4,15 +4,28 @@ const initialState = {
     cart: []
 };
 
+
 const productReducer = (state = initialState, action) => {
+    const selectedProduct = state.cart.find
+        ((product) => product._id === action.payload._id)
+
+    console.log(selectedProduct)
+
     switch (action.type) {
         case ADD_TO_CART:
+            if (selectedProduct) {
+                return state;
+            }
             return {
                 ...state,
-                cart: [...state.cart, action.payload]
+                cart: [...state.cart, { ...action.payload, quantity: 1 }]
             };
         case REMOVE_TO_CART:
-            return {};
+            return {
+                ...state,
+                cart: state.cart.filter(
+                    (product) => product._id !== action.payload._id)
+            };
         default:
             return state;
     }
