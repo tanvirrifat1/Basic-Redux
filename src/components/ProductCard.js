0b6dc55042e4b25ac9e74742afc468ab80241579
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-02-08 11:23:06 +0600

    added cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
-import { useProducts } from "../context/ProductProvider";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actionCreators/productActions";
+import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 import { actionTypes } from "../state/ProductState/actionTypes";
 
 const ProductCard = ({ product }) => {
-  const { dispatch } = useProducts();
+
+  const dispatch = useDispatch();
 
   return (
     <div
@@ -19,17 +22,14 @@ const ProductCard = ({ product }) => {
       <div className=' flex-1'>
         <ul className='space-y-2'>
           {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+            return <li key={feature._id} className='text-sm '>{feature}</li>;
           })}
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
         <button
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          onClick={() =>
-            dispatch({ type: actionTypes.ADD_TO_CART, payload: product })
-          }
-        >
+          onClick={() => dispatch(addToCart(product))}
+          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
         </button>
         <button
